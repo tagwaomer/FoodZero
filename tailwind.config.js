fc20ecce7f684d2dfcf33d6c9de161a78ba06ad4
tagwa-omer <tagwaomer95@gmail.com>
2026-02-27 23:28:51 +0400

    Greate FoodZero project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#233000",
+        foreground: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+}
